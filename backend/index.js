46e555d5df0b4dc84d0f9232ef8cb7719c1650d8
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2025-03-23 13:44:27 +0530

    commite

---
+++ backend/index.js
@@ -0,0 +1,52 @@
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+// // For testing the TaskModel console_log
+// const taskModel = require("./models/task.model");
+
+
+// // For testing the TaskService console_log
+// const TaskService = require("./services/task.service");
+
+// const TaskServiceInstance = new TaskService();
+// console.log(
+//   TaskServiceInstance.find,
+//   TaskServiceInstance.create,
+//   TaskServiceInstance.update,
+//   TaskServiceInstance.delete
+// );
+
+// // To confirm controller module
+// const {getTasks, createTask, updateTask, deleteTask,} = require("./controllers/task.controller");
+// console.log(getTasks, createTask, updateTask, deleteTask);
+
+
+
+
+
+const app = express();
+const PORT = 8082;
+const DB_URI = "mongodb://localhost:27017/task-manager";
+//const DB_URI = "mongodb+srv://kamleshsahur:AgZ4khdKvxWZHurP@tasks.v79mv.mongodb.net/?retryWrites=true&w=majority&appName=tasks";
+
+mongoose
+    .connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected!"))
+    .catch((error) => console.log("Error in connecting DB", error));
+
+
+app.use(cors());
+app.use(express.json());
+
+const taskRoutes = require("./routes/task.routes");
+app.use("/tasks", taskRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Backend listning on Port ${PORT}`);
+});
